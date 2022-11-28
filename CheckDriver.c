a0ac2f95bfb80569be28d882384f04b2c30e6cdc
Liam Aslan <79198511+LiamAs05@users.noreply.github.com>
2022-11-28 22:56:39 +0200

    Added more code that does not work, need to manage without winternal

--- CheckDriver.c
+++ CheckDriver.c
@@ -1,4 +1,30 @@
+#include <winternl.h>
 #include <ntddk.h>
+
+void print_to_debugger()
+{
+	DbgPrint("Hello from the kernel :)\nWith great power comes great responsibility!\n");
+}
+
+void list_all_procs()
+{
+	SYSTEM_PROCESS_INFORMATION s[100];
+	NTSTATUS status = NtQuerySystemInformation(SystemProcessInformation, &s, sizeof(SYSTEM_PROCESS_INFORMATION) * 100, NULL);
+	
+	if (status != STATUS_SUCCESS)
+	{
+		DbgPrint("This whole operation is a failue, status: (0x%08X)\n", status);
+		return;
+	}
+
+	for (int i = 0; i < 100; i++)
+	{
+		if (!s->NextEntryOffset)
+			break;
+
+		DbgPrint("Process %d with name %s", s->UniqueProcessId, s->ImageName);
+	}
+}
 
 VOID DriverUnload(PDRIVER_OBJECT DriverObject) {
 	// delete device object
@@ -11,7 +37,7 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) 
 	DriverObject->DriverUnload = DriverUnload;
 
 	UNICODE_STRING dev_name = RTL_CONSTANT_STRING(L"\\Device\\CoolDev");
-
+	
 	PDEVICE_OBJECT device_object;
 	NTSTATUS status = IoCreateDevice(
 		DriverObject,
@@ -28,12 +54,7 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) 
 		return status;
 	}
 
-
-	DbgPrint("Hello from the kernel :)\nWith great power comes great responsibility!");
-	PEPROCESS s = PsGetCurrentProcess();
-	
-	DbgPrint();
-	
+	print_to_debugger();
 
 	return STATUS_SUCCESS;
 }
